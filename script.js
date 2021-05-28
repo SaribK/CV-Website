90e52b81298d3ce25e40c19b6a7df7cd6fe87e84
Sarib Kashif <kashis2@mcmaster.ca>
2021-05-28 16:06:12 -0400

    finished projects section

--- script.js
+++ script.js
@@ -77,7 +77,66 @@ $(document).ready(function(){
             $(".navbar").hide();
             document.getElementById("popup-2").style.width = "100%";
             disableScroll();
-            //TODO maybe add a popup effect
+        }
+        else if ($this.hasClass('proj3')) {
+            $(".navbar").hide();
+            document.getElementById("popup-3").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj4')) {
+            $(".navbar").hide();
+            document.getElementById("popup-4").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj5')) {
+            $(".navbar").hide();
+            document.getElementById("popup-5").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj6')) {
+            $(".navbar").hide();
+            document.getElementById("popup-6").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj7')) {
+            $(".navbar").hide();
+            document.getElementById("popup-7").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj8')) {
+            $(".navbar").hide();
+            document.getElementById("popup-8").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj9')) {
+            $(".navbar").hide();
+            document.getElementById("popup-9").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj10')) {
+            $(".navbar").hide();
+            document.getElementById("popup-10").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj11')) {
+            $(".navbar").hide();
+            document.getElementById("popup-11").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj12')) {
+            $(".navbar").hide();
+            document.getElementById("popup-12").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj13')) {
+            $(".navbar").hide();
+            document.getElementById("popup-13").style.width = "100%";
+            disableScroll();
+        }
+        else if ($this.hasClass('proj14')) {
+            $(".navbar").hide();
+            document.getElementById("popup-14").style.width = "100%";
+            disableScroll();
         }
       });
 });
@@ -94,6 +153,77 @@ function hidePopup2() {
     enableScroll();
 }
 
+function hidePopup3() {
+    document.getElementById("popup-3").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup4() {
+    document.getElementById("popup-4").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup5() {
+    document.getElementById("popup-5").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup6() {
+    document.getElementById("popup-6").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup7() {
+    document.getElementById("popup-7").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup8() {
+    document.getElementById("popup-8").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup9() {
+    document.getElementById("popup-9").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup10() {
+    document.getElementById("popup-10").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup11() {
+    document.getElementById("popup-11").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup12() {
+    document.getElementById("popup-12").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup13() {
+    document.getElementById("popup-13").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
+
+function hidePopup14() {
+    document.getElementById("popup-14").style.width = "0%";
+    $(".navbar").show();
+    enableScroll();
+}
 
 //disable scrolling
 function disableScroll() {
